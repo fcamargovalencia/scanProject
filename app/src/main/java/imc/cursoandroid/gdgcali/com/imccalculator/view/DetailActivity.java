b2e903cf5c3327bda408a7941d07e96da5973a28
Jose Berna <jberna@ptk.com.co>
2016-08-13 11:08:15 -0500

    developer: fixed scann cam

--- app/src/main/java/imc/cursoandroid/gdgcali/com/imccalculator/view/DetailActivity.java
+++ app/src/main/java/imc/cursoandroid/gdgcali/com/imccalculator/view/DetailActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -98,14 +99,15 @@ public class DetailActivity extends Activity {
                         if (scanResult != null) {
                             String resultContents = scanResult.getContents();
                             if (resultContents != null) {
-                                Toast.makeText(context, "Escaneado: " + resultContents, Toast.LENGTH_LONG);
+                                Toast.makeText(context, "Escaneado: " + resultContents, Toast.LENGTH_LONG).show();
+                                Log.d("Escann", resultContents);
                             }
                         }
                     }
                     break;
             }
         } catch (Exception e) {
-
+            Toast.makeText(context, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
 }
